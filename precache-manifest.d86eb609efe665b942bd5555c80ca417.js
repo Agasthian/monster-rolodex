b05068f47592dff52dc8cf4c69080fb3fc14430a
Agasthian <agasthian_r@hotmail.com>
2019-08-04 15:17:54 +0530

    Updates

---
+++ precache-manifest.d86eb609efe665b942bd5555c80ca417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87555469f6584c44f78baa39c855eb99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3d2dede8d7ceb814405",
+    "url": "/static/css/main.4ec49ff4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c3d2dede8d7ceb814405",
+    "url": "/static/js/main.91ad3935.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
